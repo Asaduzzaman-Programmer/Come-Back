8d0347fdfcb736d43f162d7accd6bf5f4c977f43
asad <asaduzzamanprogrammer@gmail.com>
2024-02-25 18:34:22 +0600

    chaool

--- come.js
+++ come.js
@@ -9,16 +9,4 @@ function add (a, b){
 
 const sum = add(7, 8)
 
-
-function add (a, b){
-    const result = a + b ;
-   console.log(result)
-}
-
-const sumGH = add(7, 8)
-function add (a, b){
-    const result = a + b ;
-   console.log(result)
-}
-
-const sumGHk = add(7, 8)
+console.log(sum)
