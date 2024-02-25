c3e13411f0f821d56e7e600ddb9f9b4304165960
asad <asaduzzamanprogrammer@gmail.com>
2024-02-25 18:31:56 +0600

    chal

--- come.js
+++ come.js
@@ -1,1 +1,18 @@
 // start a new journey
+
+
+
+function add (a, b){
+    const result = a + b ;
+   console.log(result)
+}
+
+const sum = add(7, 8)
+
+
+function add (a, b){
+    const result = a + b ;
+   console.log(result)
+}
+
+const sumGH = add(7, 8)
