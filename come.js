e0670bedb99cbf457174539ff777dd52d2737f92
asad <asaduzzamanprogrammer@gmail.com>
2024-02-27 19:58:58 +0600

    koooopoi

--- come.js
+++ come.js
@@ -66,6 +66,12 @@ function dum (c, d){
     const back = c + d;
     console.log(back)
 }
+const dum = cum(5, 8)
+
+function dum (c, d){
+    const back = c + d;
+    console.log(back)
+}
 
 console.log("no one can beat me");
 // console.log("no one can beat me no one");
