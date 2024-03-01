38723a65c0db21e75ac208e9b9aadcae00c5b40c
asad <asaduzzamanprogrammer@gmail.com>
2024-03-01 16:44:46 +0600

    no4

---
+++ undefiend.js
@@ -0,0 +1,24 @@
+// 8 ways to get undifeind
+
+// no 1
+let fist;
+console.log(fist);
+
+// no 2
+function call() {
+    const human = "sawon"
+    // return human if it open this is defiend
+}
+
+const result = call();
+console.log(result);
+
+// no 3
+function third(a, b, c, d) {
+    const res = a + b + c + d;
+    return res
+}
+const re = third(2, 2, 2, );
+console.log(re);
+
+// no 4
