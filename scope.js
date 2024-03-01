8ef7eca969b2e0cf7ee63e931bbd6fca08cb5d99
asad <asaduzzamanprogrammer@gmail.com>
2024-03-02 00:46:11 +0600

    close

---
+++ scope.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    const total = a + b;
+    console.log(a, b);
+    // console.log(total);
+   
+    if (b > 5) {
+        const sum = 25 + a + b
+        console.log(sum);
+    }
+    else{
+        const sum = 5+a+b
+        var current = sum;
+    }
+    console.log(current);
+    return total;
+}
+
+add(5, 3)
--- triple.js
+++ triple.js
@@ -1,8 +1,11 @@
 const first = 2;
-const second = "2";
-if(first === second){
+const second = true;
+if(first == second){
     console.log("Value is eqal")
 }
 else{
     console.log("Value is not eqal");
 }
+
+const fun = false + false;
+console.log(fun);
