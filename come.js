361bb1817e773a2767cf69ad3dea77dd6b7425ff
asad <asaduzzamanprogrammer@gmail.com>
2024-03-01 16:20:34 +0600

    test

--- come.js
+++ come.js
@@ -3,4 +3,10 @@
  const ages = [45, 45, 45];
  const student = {id: 45, class:7};
  console.log(typeof a, typeof b, typeof ages, typeof student);
+
+ let x = 7;
+ let y = x;
+ console.log(x, y)
+ y = 5;
+ console.log(x, y)
  
