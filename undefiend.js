1a15ae72719e38804364e00b44cd7596c288ffc8
asad <asaduzzamanprogrammer@gmail.com>
2024-03-01 16:54:29 +0600

    vai+ami practice

--- undefiend.js
+++ undefiend.js
@@ -18,7 +18,29 @@ function third(a, b, c, d) {
     const res = a + b + c + d;
     return res
 }
-const re = third(2, 2, 2, );
+const re = third(2, 2, 2,);
 console.log(re);
 
 // no 4
+function noNegative(a, b) {
+    if (a < 0 || b < 0) {
+        return
+    }
+    else {
+        const fol = a + b;
+        console.log(fol);
+    }
+        
+    
+}
+noNegative(2,-21)
+
+// no 4/2
+function neg(a, b){
+    if(a<0 || b<0){
+        return
+    }
+    return a+b;
+}
+const don =neg(4,4);
+console.log(don);
